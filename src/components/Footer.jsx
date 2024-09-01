83dd6b1f394e3fbb77553155a099b4cf704dc5f5
Alec Taber <alectaber12@gmail.com>
2024-09-01 13:34:24 -0700

    add links to github, linkedin, and indeed to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,33 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="bg-blue-600 text-white p-4 mt-4 text-center">
-            <a href="GitHub">GitHub</a>
-            <a href="LinkedIn">LinkedIn</a>
-            <a href="Indeed">Indeed</a>
+            <div className="flex justify-center space-x-4 mb-2">
+                <a
+                    href="https://GitHub.com/AlecTaber"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                >
+                    GitHub
+                </a>
+                <a
+                    href="www.linkedin.com/in/alec-taber-11b963311"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                >
+                    LinkedIn
+                </a>
+                <a
+                    href="https://profile.indeed.com/p/alect-37w1b45"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                >
+                    Indeed
+                </a>
+
+            </div>
             <p>&copy; 2024 Alec Taber</p>
         </footer>
     );
